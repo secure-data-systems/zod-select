73be96bec0cba89591cc6f90a962804dd9d00fa7
Greg Kimmy <greg.kimmy@gmail.com>
2025-06-10 23:06:00 -0500

    chore: updated eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,13 @@
-import synergy from '@securedatasystems/eslint-config';
+import securedatasystems from '@securedatasystems/eslint-config';
 
 export default [
-	...synergy.configs.flat,
+	...securedatasystems.configs.flat,
 	{
 		ignores: [
 			'**/dist/*',
 			'**/build/*',
 			'**/scripts/*',
-			'**/*-dbschema.js',
-			'commitlint.config.js',
 			'eslint.config.mjs',
-			'release.config.mjs',
 			'test.ts',
 			'tsconfig.json'
 		]
